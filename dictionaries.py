43a7809bcd1cf0fda3518bda545824d76d00e9ac
sudharaD <sudharad7@gmail.com>
2022-12-27 23:05:16 +0530

    Dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,34 @@
+programming_dictionary = {
+    "Bug": "An error in a program that prevents the program from running as expected.",
+    "Function": "A piece of code that you can easily call over and over again.",
+}
+
+print(programming_dictionary)
+
+# Add item to a dictionary
+programming_dictionary["Loop"] = "The action of doing something over and over again."
+
+print(programming_dictionary)
+
+print(programming_dictionary["Bug"])
+
+print(["A", "B"][0])
+
+# defining dictionary
+empty_dictionary = {}
+
+# Edit dictionary
+programming_dictionary["Loop"] = "Loop is loop"
+
+print(programming_dictionary)
+
+# Clearing dictionary
+programming_dictionary = {}
+
+print(programming_dictionary)
+
+letter_dictionary = {"A": "a", "B": "b", "C": "c"}
+
+for i in letter_dictionary:
+    print(i)
+    print(letter_dictionary[i])
