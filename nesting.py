f5360840bf57a4fb88b4424b82601892e4c35859
sudharaD <sudharad7@gmail.com>
2022-12-28 22:22:28 +0530

    nesting

---
+++ nesting.py
@@ -0,0 +1,22 @@
+# List & dictionaries inside a dictionary
+travel_log = {
+    "France": {"Cities_Visited" : ["Paris", "Lille", "Dijon"], "Total_Visits" : 12},
+    "Germany": ["Berlin", "Hamburg", "Stuttgart"],
+}
+
+# List incide list
+["A", ["B", "C"]]
+
+# Nesting Dictionaries inside a list
+travel_log = [
+    {
+        "Country" :"France", 
+        "Cities_Visited" : ["Paris", "Lille", "Dijon"], 
+        "Total_Visits" : 12
+    },
+    {
+        "Country" :"France", 
+        "Cities_Visited" : ["Paris", "Lille", "Dijon"], 
+        "Total_Visits" : 5
+    },
+]
